a8e9a02c9410d108cfa82d102efda565e437e0b8
Pavan097716 <pavanvallu185@gmail.com>
2025-03-24 06:48:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 
-API_ID = "" #API ID get it from telegram.org
-API_HASH = "" #API HASH get it from telegram.org
-BOT_TOKEN = "" #Bot Token get it from @BotFather
-URL_A = "" #News RSS Feed Url 2
-#URL_B = "" #News RSS Feed Url 2
-START_PIC = "" #Startup Pic
-MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
-ADMINS = "" #Admin ID
+API_ID = "20793620" #API ID get it from telegram.org
+API_HASH = "a712d2b8486f26c4dee5127cc9ae0615" #API HASH get it from telegram.org
+BOT_TOKEN = "7488650724:AAELNIj6CCnUdRG25GO7HtDQLdeasG8mm6g" #Bot Token get it from @BotFather
+URL_A = "http://myanimelist.net/rss.php?type=news" #News RSS Feed Url 2
+URL_B = "https://www.crunchyroll.com" #News RSS Feed Url 2
+START_PIC = "http://postimg.cc/68vfZ3c4" #Startup Pic
+MONGO_URI = "mongodb+srv://pokemonchannel098:yaE7BvFwWIXdb3HQ@cluster0.gdr57.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" #Databse uri get it from https://www.mongodb.com/
+ADMINS = "6853851676 6186511950" #Admin ID
